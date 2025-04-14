12b08bac28e0c9b92b54c63f25c3472cc37009fb
Bruna Belini <brunabbelini1@hotmail.com>
2025-04-14 17:52:39 -0300

    Creating the Category and increasing the Contact in the database

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -9,6 +16,9 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    pictures = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
